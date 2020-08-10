5502e429cf3e0ee74078bc80712ee57a72f8b015
Jani Anttonen <jantto@utu.fi>
2020-08-10 14:54:32 +0300

    Update fetcher script

--- run_fetcher.sh
+++ run_fetcher.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-docker run --rm -v data:/home/python/app/data eqlabs/crypto-sanity-preserver env MAX_INDEX=250 env RESULTS_PER_PAGE=50 /bin/sh ./fetch.sh
+docker run --rm -v data:/home/python/app/data -v thumbs:/home/python/app/static/thumbs eqlabs/crypto-sanity-preserver env MAX_INDEX=250 env RESULTS_PER_PAGE=50 /bin/sh ./fetch.sh
 docker restart sanity
